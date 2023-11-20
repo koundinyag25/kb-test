d94247d3c713713d51a66e319f0ddbaed877ebdf
Koundinya Gavicherla <koundinya1991@gmail.com>
2023-11-20 20:10:08 +0530

    Add files via upload

--- PanelItem.js
+++ PanelItem.js
@@ -1,18 +1,18 @@
 import React, { useState } from 'react';
 
-export const PanelItem = ({ item }) => {
-    console.log({ itemChilds: item.childItems});
+export const PanelItem = ({ item, map }) => {
     const [collapse, setCollapse] = useState(false);
     if(!item) return null;
-  
+    const childItems = map[item.id]?.childItems || [];
+
     return (
       <>
         <span>
-         {item?.childItems?.length > 0 && <button onClick={() => setCollapse(!collapse)}>{collapse ? '+': '-'}</button>}
+         {childItems.length > 0 && <button onClick={() => setCollapse(!collapse)}>{collapse ? '+': '-'}</button>}
           {item.name}
         </span>
         <div className='left'>
-          {!collapse && item.childItems && item?.childItems.length > 0 ? item?.childItems.map(child => <PanelItem item={child} />) : null}
+          {!collapse && childItems.length > 0 ? childItems.map(child => <PanelItem item={child} map={map}/>) : null}
         </div>
       </>
     )
